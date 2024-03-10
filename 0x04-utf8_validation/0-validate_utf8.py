e72e400175d0b259fad91213898d3a9b0f5f5460
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-10 23:32:40 +0000

    fix(utf8-validation): validUTF8 function

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,36 +4,26 @@ utf8 validation
 """
 
 
-def is_valid(c):
-    """
-    check single value
-    """
-    if c <= 0x7F:
-        return True
-    if 0xC080 == c:
-        return True
-
-    if 0xC280 <= c and c <= 0xDFBF:
-        return (c & 0xE0C0) == 0xC080
-
-    if 0xEDA080 <= c and c <= 0xEDBFBF:
-        return False
-
-    if 0xE0A080 <= c and c <= 0xEFBFBF:
-        return ((c & 0xF0C0C0) == 0xE08080)
-
-    if 0xF0908080 <= c and c <= 0xF48FBFBF:
-        return (c & 0xF8C0C0C0) == 0xF0808080
-
-    return False
-
-
 def validUTF8(data):
     """
     check all list contains valid utf8
     """
+    remaining_bytes = 0
 
-    for c in data:
-        if not is_valid(c):
-            return False
-    return True
+    for byte in data:
+        byte = 255 & byte
+        if remaining_bytes == 0:
+            if byte >> 5 == 0b110:
+                remaining_bytes = 1
+            elif byte >> 4 == 0b1110:
+                remaining_bytes = 2
+            elif byte >> 3 == 0b11110:
+                remaining_bytes = 3
+            elif byte >> 7:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            remaining_bytes -= 1
+
+    return remaining_bytes == 0
